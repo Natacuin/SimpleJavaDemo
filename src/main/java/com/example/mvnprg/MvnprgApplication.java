0237eb92afb830b4fa24474be98145451c8b05ac
Melvin 3PG <melvin.fallas@3pillarglobal.com>
2022-10-12 09:52:10 -0600

    Creating simple basic maven project.

--- src/main/java/com/example/mvnprg/MvnprgApplication.java
+++ src/main/java/com/example/mvnprg/MvnprgApplication.java
@@ -1,13 +1,13 @@
-package bac.bel.demo;
+package com.example.mvnprg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MvnprgApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MvnprgApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/mvnprg/MvnprgApplicationTests.java
+++ src/test/java/com/example/mvnprg/MvnprgApplicationTests.java
@@ -1,10 +1,10 @@
-package bac.bel.demo;
+package com.example.mvnprg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MvnprgApplicationTests {
 
 	@Test
 	void contextLoads() {
