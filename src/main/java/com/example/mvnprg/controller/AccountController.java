0467ed5f6bb48f4aa9a8f85d118b752ce0ae1cfb
Melvin 3PG <melvin.fallas@3pillarglobal.com>
2022-10-20 17:15:02 -0600

    Making copy of PetApiV3.yaml file. Adding AccountController for testing.

---
+++ src/main/java/com/example/mvnprg/controller/AccountController.java
@@ -0,0 +1,19 @@
+//package com.example.mvnprg.controller;
+//
+//import com.example.mvnprg.openapi.api.AccountApi;
+//import com.example.mvnprg.openapi.model.OnQueryAccountResponse;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+//@RestController
+//public class AccountController implements AccountApi {
+//
+//	@Override
+//	public ResponseEntity<OnQueryAccountResponse> getAccountByAccountNumber(Long accountNumber) {
+//		// TODO Auto-generated method stub
+//		return AccountApi.super.getAccountByAccountNumber(accountNumber);
+//	}
+//	
+//}
